95627cd8efa57ea33ec72df9a0f11cfb2255dccd
Rüdiger Klaehn <rklaehn@gmail.com>
2019-11-18 22:19:25 +0100

    More benches

--- benches/set_comparison.rs
+++ benches/set_comparison.rs
@@ -2,26 +2,64 @@ use vec_collections::*;
 use std::collections::{BTreeSet, HashSet};
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-fn vec_set_create(n: u64) -> usize {
+fn vs_create(n: u64) -> usize {
     let res: VecSet<u64> = (0..n).collect();
     res.len()
 }
 
-fn btree_set_create(n: u64) -> usize {
+fn vs_contains(x: &VecSet<u64>, n: u64) -> usize {
+    let mut res = 0;
+    for e in 0..n {
+        if x.contains(&e) {
+            res += 1;
+        }
+    }
+    res
+}
+
+fn bs_create(n: u64) -> usize {
     let res: BTreeSet<u64> = (0..n).collect();
     res.len()
 }
 
-fn hash_set_create(n: u64) -> usize {
+fn bs_contains(x: &BTreeSet<u64>, n: u64) -> usize {
+    let mut res = 0;
+    for e in 0..n {
+        if x.contains(&e) {
+            res += 1;
+        }
+    }
+    res
+}
+
+fn hs_create(n: u64) -> usize {
     let res: HashSet<u64> = (0..n).collect();
     res.len()
 }
 
-pub fn creation(c: &mut Criterion) {
-    c.bench_function("vec_set_create 100", |b| b.iter(|| vec_set_create(black_box(100))));
-    c.bench_function("btree_set_create 100", |b| b.iter(|| btree_set_create(black_box(100))));
-    c.bench_function("hash_set_create 100", |b| b.iter(|| hash_set_create(black_box(100))));
+fn hs_contains(x: &HashSet<u64>, n: u64) -> usize {
+    let mut res = 0;
+    for e in 0..n {
+        if x.contains(&e) {
+            res += 1;
+        }
+    }
+    res
 }
 
-criterion_group!(benches, creation);
+pub fn creation(c: &mut Criterion) {
+    c.bench_function_over_inputs("vs_create", |b, i| b.iter(|| vs_create(black_box(**i))), [100u64, 200].iter());
+    c.bench_function_over_inputs("bs_create", |b, i| b.iter(|| bs_create(black_box(**i))), [100u64, 200].iter());
+    c.bench_function_over_inputs("hs_create", |b, i| b.iter(|| hs_create(black_box(**i))), [100u64, 200].iter());
+}
+pub fn lookup(c: &mut Criterion) {
+    let vs: VecSet<u64> = (0..100).collect();
+    let bs: BTreeSet<u64> = (0..100).collect();
+    let hs: HashSet<u64> = (0..100).collect();
+    c.bench_function("vs_lookup 100", |b| b.iter(|| vs_contains(black_box(&vs), black_box(200))));
+    c.bench_function("bs_lookup 100", |b| b.iter(|| bs_contains(black_box(&bs), black_box(200))));
+    c.bench_function("hs_lookup 100", |b| b.iter(|| hs_contains(black_box(&hs), black_box(200))));
+}
+
+criterion_group!(benches, creation, lookup);
 criterion_main!(benches);
