ed08e3e5b1e0083cb9de12dce59dc17bf901024b
Rüdiger Klaehn <rklaehn@protonmail.com>
2020-08-28 18:23:06 +0200

    Better benchmarks

--- benches/set_comparison.rs
+++ benches/set_comparison.rs
@@ -1,83 +1,91 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, Criterion, BenchmarkId};
 use std::collections::{BTreeSet, HashSet};
 use vec_collections::*;
 
-fn vs_create(n: u64) -> usize {
-    let res: VecSet2<u64> = (0..n).map(|x| n - x).collect();
+fn vs_create(v: &Vec<u64>) -> usize {
+    let res: VecSet2<u64> = v.iter().cloned().collect();
     res.len()
 }
 
-fn vs_contains(x: &VecSet2<u64>, n: u64) -> usize {
+fn vs_contains(x: &VecSet2<u64>, values: &[u64]) -> usize {
     let mut res = 0;
-    for e in 0..n {
-        if x.contains(&e) {
+    for e in values {
+        if x.contains(e) {
             res += 1;
         }
     }
     res
 }
 
-fn bs_create(n: u64) -> usize {
-    let res: BTreeSet<u64> = (0..n).map(|x| n - x).collect();
+fn bs_create(v: &Vec<u64>) -> usize {
+    let res: BTreeSet<u64> = v.iter().cloned().collect();
     res.len()
 }
 
-fn bs_contains(x: &BTreeSet<u64>, n: u64) -> usize {
+fn bs_contains(x: &BTreeSet<u64>, values: &[u64]) -> usize {
     let mut res = 0;
-    for e in 0..n {
-        if x.contains(&e) {
+    for e in values {
+        if x.contains(e) {
             res += 1;
         }
     }
     res
 }
 
-fn hs_create(n: u64) -> usize {
-    let res: HashSet<u64> = (0..n).map(|x| n - x).collect();
+fn hs_create(v: &Vec<u64>) -> usize {
+    let res: HashSet<u64> = v.iter().cloned().collect();
     res.len()
 }
 
-fn hs_contains(x: &HashSet<u64>, n: u64) -> usize {
+fn hs_contains(x: &HashSet<u64>, values: &[u64]) -> usize {
     let mut res = 0;
-    for e in 0..n {
-        if x.contains(&e) {
+    for e in values {
+        if x.contains(e) {
             res += 1;
         }
     }
     res
 }
+use rand::seq::SliceRandom;
+use rand::SeedableRng;
 
 pub fn creation(c: &mut Criterion) {
-    c.bench_function_over_inputs(
-        "vs_create",
-        |b, i| b.iter(|| vs_create(black_box(**i))),
-        [100u64, 200].iter(),
-    );
-    c.bench_function_over_inputs(
-        "bs_create",
-        |b, i| b.iter(|| bs_create(black_box(**i))),
-        [100u64, 200].iter(),
-    );
-    c.bench_function_over_inputs(
-        "hs_create",
-        |b, i| b.iter(|| hs_create(black_box(**i))),
-        [100u64, 200].iter(),
-    );
+    let mut group = c.benchmark_group("creation");
+    let mut rand = rand::rngs::StdRng::from_seed([0u8; 32]);
+    for i in (100u64..=1000).step_by(100) {
+        let mut values = (0..i).collect::<Vec<_>>();
+        values.shuffle(&mut rand);
+
+        group.bench_with_input(BenchmarkId::new("vs_create", i), &values, 
+            |b, values| b.iter(|| vs_create(black_box(values))));
+
+        group.bench_with_input(BenchmarkId::new("bs_create", i), &values, 
+            |b, values| b.iter(|| bs_create(black_box(values))));
+
+        group.bench_with_input(BenchmarkId::new("hs_create", i), &values, 
+            |b, values| b.iter(|| hs_create(black_box(values))));
+    }
 }
 pub fn lookup(c: &mut Criterion) {
-    let vs: VecSet2<u64> = (0..100).collect();
-    let bs: BTreeSet<u64> = (0..100).collect();
-    let hs: HashSet<u64> = (0..100).collect();
-    c.bench_function("vs_lookup 100", |b| {
-        b.iter(|| vs_contains(black_box(&vs), black_box(200)))
-    });
-    c.bench_function("bs_lookup 100", |b| {
-        b.iter(|| bs_contains(black_box(&bs), black_box(200)))
-    });
-    c.bench_function("hs_lookup 100", |b| {
-        b.iter(|| hs_contains(black_box(&hs), black_box(200)))
-    });
+    let mut group = c.benchmark_group("lookup");
+    let mut rand = rand::rngs::StdRng::from_seed([0u8; 32]);
+    for i in (100u64..=10000).step_by(100) {
+        let mut values = (0..i).collect::<Vec<_>>();
+        values.shuffle(&mut rand);
+
+        let coll: VecSet2<u64> = values.iter().cloned().collect();
+        group.bench_with_input(BenchmarkId::new("vs_lookup", i), &(coll, &values[0..10]),
+            |b, coll| b.iter(|| vs_contains(black_box(&coll.0), &coll.1)));
+
+        let coll: BTreeSet<u64> = values.iter().cloned().collect();
+        group.bench_with_input(BenchmarkId::new("bs_lookup", i), &(coll, &values[0..10]), 
+        |b, coll| b.iter(|| bs_contains(black_box(&coll.0), &coll.1)));
+
+        let coll: HashSet<u64> = values.iter().cloned().collect();
+        group.bench_with_input(BenchmarkId::new("hs_lookup", i), &(coll, &values[0..10]), 
+        |b, coll| b.iter(|| hs_contains(black_box(&coll.0), &coll.1)));
+    }
 }
 
-criterion_group!(benches, creation, lookup);
+criterion_group!(benches, /* creation, */ lookup);
 criterion_main!(benches);
