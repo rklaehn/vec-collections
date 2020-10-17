cccba9cb807ffae30cabee4a5cd7184ad6bd2b5c
Ruediger Klaehn <rklaehn@protonmail.com>
2020-10-17 10:48:13 +0200

    Add test for empty case

--- src/macros.rs
+++ src/macros.rs
@@ -1,5 +1,8 @@
 #[macro_export]
 macro_rules! vecset {
+    () => ({
+        $crate::VecSet::default()
+    });
     ($($x:expr),*$(,)*) => ({
         let mut set = $crate::VecSet::default();
         $(set.insert($x);)*
@@ -27,8 +30,10 @@ mod tests {
 
     #[test]
     fn vecset_macro() {
+        let from_macro_empty: VecSet<[u32; 4]> = vecset! {};
         let from_macro: VecSet<[u32; 4]> = vecset! {1,2,3};
         let mut manual: VecSet<[u32; 4]> = VecSet::default();
+        assert_eq!(from_macro_empty, manual);
         manual.insert(1);
         manual.insert(2);
         manual.insert(3);
@@ -37,8 +42,10 @@ mod tests {
 
     #[test]
     fn vecmap_macro() {
+        let from_macro_empty: VecMap<[(u32, u32); 4]> = vecmap! {};
         let from_macro: VecMap<[(u32, u32); 4]> = vecmap! {1 => 2, 2 => 4,3 => 6};
         let mut manual: VecMap<[(u32, u32); 4]> = VecMap::default();
+        assert_eq!(from_macro_empty, manual);
         manual.insert(1, 2);
         manual.insert(2, 4);
         manual.insert(3, 6);
